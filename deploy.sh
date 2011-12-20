627b6985f42542ef6283847b765e52b9eb65e445
nagaseyasuhito <nagase@nagaseyasuhito.net>
2011-12-20 13:46:44 +0900

    modify deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn clean release:prepare release:perform beanstalk:upload-source-bundle beanstalk:create-application-version beanstalk:update-environment beanstalk:clean-previous-versions -Dbeanstalk.versionsToKeep=2 -Dbeanstalk.dryRun=false
+mvn clean release:prepare release:perform -Dbeanstalk.versionsToKeep=2 -Dbeanstalk.dryRun=false
